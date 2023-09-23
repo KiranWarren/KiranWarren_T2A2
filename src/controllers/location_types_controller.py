58ca41c678817d2be4404ef0a4576f0f0e7842e1
Kiran Warren <kiran.m.warren@gmail.com>
2023-09-23 18:13:17 +1000

    Added all location type routes

--- src/controllers/location_types_controller.py
+++ src/controllers/location_types_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, request
 
 from main import db
 from models.location_types import LocationType
@@ -6,6 +6,48 @@ from schemas.location_type_schema import location_type_schema, location_types_sc
 
 location_types = Blueprint('location_type', __name__, url_prefix="/location-types")
 
+# CREATE a location type
+# /location-types/
+@location_types.route("/", methods=["POST"])
+def create_location_type():
+    '''
+    
+    '''
+    location_type_json = location_type_schema.load(request.json)
+    new_location_type = LocationType(**location_type_json)
+
+    db.session.add(new_location_type)
+    db.session.commit()
+
+    return jsonify(location_type_schema.dump(new_location_type))
+
+
+# UPDATE a location_type by id
+# /location-types/<id>
+@location_types.route("/<int:location_type_id>", methods=["PUT"])
+def update_location_type_by_id(location_type_id: int):
+    '''
+    
+    '''
+    query = db.select(LocationType).filter_by(id=location_type_id)
+    location_type = db.session.scalar(query)
+    response = location_type_schema.dump(location_type)
+
+    if response:
+        location_type_json = location_type_schema.load(request.json)
+
+        # Map request json to location_type
+        location_type.location_type = location_type_json["location_type"]
+
+        # Commit changes
+        db.session.commit()
+        return jsonify(location_type_schema.dump(location_type))
+
+    return abort(404, description=f"A location_type with `id`={location_type_id} does not exist in the database. No updates have been made.")
+
+
+# GET all location types
+# /location-types/
 @location_types.route("/", methods=["GET"])
 def get_location_types_list():
     query = db.select(LocationType)
@@ -13,3 +55,24 @@ def get_location_types_list():
     response = location_types_schema.dump(location_type_list)
 
     return jsonify(response)
+
+
+# DELETE a location_type by id
+# /location-types/<id>
+@location_types.route("/<int:location_type_id>", methods=["DELETE"])
+def delete_location_type_by_id(location_type_id: int):
+    '''
+    
+    '''
+    location_type = LocationType.query.filter_by(id=location_type_id).first()
+    response = location_type_schema.dump(location_type)
+
+    if not response:
+        return abort(404, description=f"A location_type with `id`={location_type_id} does not exist in the database. No deletions have been made.")
+
+    db.session.delete(location_type)
+    db.session.commit()
+
+    return jsonify({
+        "message": f"The location_type with id=`{location_type_id}` has been deleted successfully."
+    })
